47177f4d5d8f066c8a74773a1ef0aaae283c646d
yshrsmz <the.phantom.bane@gmail.com>
2016-05-22 15:15:52 +0900

    :construction: navigation

---
+++ app/src/main/java/net/yslibrary/mortarflowkotlin/navigation/Navigator.kt
@@ -0,0 +1,34 @@
+package net.yslibrary.mortarflowkotlin.navigation
+
+import android.content.Context
+import flow.ClassKey
+import javax.inject.Inject
+
+/**
+ * Created by yshrsmz on 2016/05/22.
+ */
+class Navigator @Inject constructor(private val context: Context) {
+  fun goTo(newTop: ClassKey) {
+
+  }
+
+  fun goBack() {
+
+  }
+
+  fun replaceTo(newTop: ClassKey) {
+
+  }
+
+  fun resetTo(newTop: ClassKey) {
+
+  }
+
+  fun takeContext(context: Context) {
+
+  }
+
+  fun dropContext(context: Context) {
+
+  }
+}
