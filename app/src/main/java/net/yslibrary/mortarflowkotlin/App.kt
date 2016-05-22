cbc1e9cedb7dedf657045856dedba88d8231e346
yshrsmz <the.phantom.bane@gmail.com>
2016-05-22 20:34:26 +0900

    add Timber

--- app/src/main/java/net/yslibrary/mortarflowkotlin/App.kt
+++ app/src/main/java/net/yslibrary/mortarflowkotlin/App.kt
@@ -4,35 +4,42 @@ import android.app.Application
 import android.content.Context
 import mortar.MortarScope
 import net.yslibrary.mortarflowkotlin.flow.DaggerService
+import timber.log.Timber
 
 /**
  * Created by a12897 on 2016/05/18.
  */
 class App : Application() {
 
-    companion object {
-        @JvmStatic fun get(context: Context) = context.applicationContext as App
-        @JvmStatic fun appComponent(context: Context) = get(context).appComponent
-        @JvmStatic fun scope(context: Context) = get(context).scope
+  companion object {
+    @JvmStatic fun get(context: Context) = context.applicationContext as App
+    @JvmStatic fun appComponent(context: Context) = get(context).appComponent
+    @JvmStatic fun scope(context: Context) = get(context).scope
+  }
+
+  private val appComponent: AppComponent by lazy {
+    DaggerAppComponent.builder()
+        .appModule(AppModule(this))
+        .build()
+  }
+
+  private val scope: MortarScope by lazy {
+    MortarScope.buildRootScope()
+        .withService(DaggerService.NAME, appComponent)
+        .build("Root")
+  }
+
+  override fun getSystemService(name: String?): Any? {
+    if (scope.hasService(name)) {
+      return scope.getService(name)
     }
 
-    private val appComponent: AppComponent by lazy {
-        DaggerAppComponent.builder()
-                .appModule(AppModule(this))
-                .build()
-    }
+    return super.getSystemService(name)
+  }
 
-    private val scope: MortarScope by lazy {
-        MortarScope.buildRootScope()
-                .withService(DaggerService.NAME, appComponent)
-                .build("Root")
-    }
+  override fun onCreate() {
+    super.onCreate()
 
-    override fun getSystemService(name: String?): Any? {
-        if (scope.hasService(name)) {
-            return scope.getService(name)
-        }
-
-        return super.getSystemService(name)
-    }
+    Timber.plant(Timber.DebugTree())
+  }
 }
