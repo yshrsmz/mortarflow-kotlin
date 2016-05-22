f92c97fba2da6c2702a152e47c2d72536abede9e
yshrsmz <the.phantom.bane@gmail.com>
2016-05-22 20:36:31 +0900

    add log

--- app/src/main/java/net/yslibrary/mortarflowkotlin/main/MainPresenter.kt
+++ app/src/main/java/net/yslibrary/mortarflowkotlin/main/MainPresenter.kt
@@ -1,9 +1,26 @@
 package net.yslibrary.mortarflowkotlin.main
 
+import android.os.Bundle
+import mortar.MortarScope
 import net.yslibrary.mortarflowkotlin.mortar.ViewPresenter
 
 /**
  * Created by yshrsmz on 2016/05/21.
  */
 class MainPresenter : ViewPresenter<MainContract.View>(), MainContract.Presenter {
+  override fun onEnterScope(scope: MortarScope?) {
+    super.onEnterScope(scope)
+  }
+
+  override fun onLoad(savedInstanceState: Bundle?) {
+    super.onLoad(savedInstanceState)
+  }
+
+  override fun onSave(outState: Bundle?) {
+    super.onSave(outState)
+  }
+
+  override fun onExitScope() {
+    super.onExitScope()
+  }
 }
--- app/src/main/java/net/yslibrary/mortarflowkotlin/mortar/ViewPresenter.kt
+++ app/src/main/java/net/yslibrary/mortarflowkotlin/mortar/ViewPresenter.kt
@@ -1,7 +1,10 @@
 package net.yslibrary.mortarflowkotlin.mortar
 
+import android.os.Bundle
+import mortar.MortarScope
 import mortar.Presenter
 import mortar.bundler.BundleService
+import timber.log.Timber
 
 /**
  * Created by yshrsmz on 2016/05/22.
@@ -9,5 +12,25 @@ import mortar.bundler.BundleService
 open class ViewPresenter<V : ContextHolder> : Presenter<V>() {
   override fun extractBundleService(view: V): BundleService? {
     return BundleService.getBundleService(view.getContext())
+  }
+
+  override fun onEnterScope(scope: MortarScope?) {
+    super.onEnterScope(scope)
+    Timber.d("onEnterScope - $this")
+  }
+
+  override fun onExitScope() {
+    super.onExitScope()
+    Timber.d("onExitScope - $this")
+  }
+
+  override fun onSave(outState: Bundle?) {
+    super.onSave(outState)
+    Timber.d("onSave - $this")
+  }
+
+  override fun onLoad(savedInstanceState: Bundle?) {
+    super.onLoad(savedInstanceState)
+    Timber.d("onLoad - $this")
   }
 }
